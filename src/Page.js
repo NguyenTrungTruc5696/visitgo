8affe283c105791237b7026ac16b10797d979da9
NguyenTrungTruc5696 <1651012210truc@ou.edu.vn>
2018-11-16 20:44:33 +0700

    message

---
+++ src/Page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
+
+class Page extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header></Header>
+                <Content></Content>
+                <Footer></Footer>
+            </div>
+        )
+    }
+}
+export default Page;
