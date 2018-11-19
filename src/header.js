4a28054b1da5045e683c558a134a127f351284ad
NguyenTrungTruc5696 <1651012210truc@ou.edu.vn>
2018-11-19 20:55:13 +0700

    essage

--- src/header.js
+++ src/header.js
@@ -1,46 +1,62 @@
 import React from 'react';
-import {Nav, Navbar, NavDropdown, NavItem, MenuItem} from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown, NavItem, MenuItem } from 'react-bootstrap'
 
- 
+
 class Header extends React.Component {
-    render() {
-        return (
-            <div>
-               <Navbar inverse collapseOnSelect>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#brand">React-Bootstrap</a>
-    </Navbar.Brand>
-    <Navbar.Toggle />
-  </Navbar.Header>
-  <Navbar.Collapse>
-    <Nav>
-      <NavItem eventKey={1} href="#">
-        Link
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">VISITGO</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                HOME
+             </NavItem>
+              <NavItem eventKey={2} href="#">
+                ABOUT
       </NavItem>
-      <NavItem eventKey={2} href="#">
-        Link
+
+              <NavItem eventKey={2} href="#">
+                SUGGESTIONS
+                </NavItem>
+
+              <NavDropdown eventKey={3} title="DANH MỤC" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}></MenuItem>
+                <MenuItem eventKey={3.2}></MenuItem>
+                <MenuItem eventKey={3.3}></MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}></MenuItem>
+              </NavDropdown>
+
+              <form className="nav navbar-form navbar-right">
+                <div className="input-group">
+                  <input type="text" class="form-control" placeholder="Search" aria-label="Search" />
+                  <div className="input-group-btn">
+                    <button className="btn btn-default" type="submit">
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                SIGN IN
       </NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-      </NavDropdown>
-    </Nav>
-    <Nav pullRight>
-      <NavItem eventKey={1} href="#">
-        Link Right
+              <NavItem eventKey={2} href="#">
+                SIGN UP
       </NavItem>
-      <NavItem eventKey={2} href="#">
-        Link Right
-      </NavItem>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-            </div>
-        )
-    }
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    )
+  }
 }
 export default Header;
